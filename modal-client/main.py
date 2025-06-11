dde50facc520db033be503a1e51590f06b8cfd67
Xiaohua Zhang <xiaohua.zhang@scale.com>
2025-06-11 20:56:58 +0000

    chore: remove token

--- modal-client/main.py
+++ modal-client/main.py
@@ -3,15 +3,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-# modal token set --token-id ak-ip216EiPfTIvIn5EuIH4Ug --token-secret as-jt6q7SR22sQdAORmQc2sxw 
 
 def main():
-    image = modal.Image.from_registry('ryan1997/agent-environment:test-4', force_build=True)
+    image = modal.Image.from_registry('ryan1997/agent-environment:test-4')
     
     app = modal.App.lookup("xiaohua-test", create_if_missing=True)
 
     # uv run python -m uvicorn agent_environment.main:app --host 0.0.0.0 --port 13788
     sandbox = modal.Sandbox.create(
+        '',
         image=image,
         timeout=50000,
         unencrypted_ports=[13788],
