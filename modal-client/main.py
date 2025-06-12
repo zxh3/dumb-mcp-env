554c117dc5c59779fd162d1561541f076497679a
Xiaohua Zhang <cheonghiuwaa@gmail.com>
2025-06-12 13:58:25 -0700

    update

--- modal-client/main.py
+++ modal-client/main.py
@@ -5,13 +5,12 @@ load_dotenv()
 
 
 def main():
-    image = modal.Image.from_registry('ryan1997/agent-environment:test-4')
-    
+    image = modal.Image.from_registry("ryan1997/agent-environment:test-4")
+
     app = modal.App.lookup("xiaohua-test", create_if_missing=True)
 
     # uv run python -m uvicorn agent_environment.main:app --host 0.0.0.0 --port 13788
     sandbox = modal.Sandbox.create(
-        '',
         image=image,
         timeout=50000,
         unencrypted_ports=[13788],
